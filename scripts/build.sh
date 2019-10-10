b689f6764100da71b439addbd6e91932cb359cd5
Akshay Krishna Upendran <akshaykrishna.upendran@gmail.com>
2019-10-10 16:11:20 +0530

    Add clang to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,3 @@
-astyle --options="./../astyle.options" "./../src/*"
+#astyle --options="./../astyle.options" "./../src/*"
+clang-format -i --verbose --style=file ./../src/*.c ./../src/*.h
 gcc ./../src/main.c -Wall -Wextra -pedantic-errors -o ./../bin/main
