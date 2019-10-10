6621e88f8fc8a5d1c4a1084e80465b4d99eb53a6
Akshay Krishna Upendran <akshaykrishna.upendran@gmail.com>
2019-10-10 16:42:50 +0530

    Change the compiler to clang.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,4 @@
 #astyle --options="./../astyle.options" "./../src/*"
 clang-format -i --verbose --style=file ./../src/*.c ./../src/*.h
-gcc ./../src/main.c -Wall -Wextra -pedantic-errors -o ./../bin/main
+#gcc ./../src/main.c -Wall -Wextra -pedantic-errors -o ./../bin/main
+clang -Werror -Weverything -pedantic-errors ./../src/main.c -o ./../bin/main
